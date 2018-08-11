d2342462822feba569538740871931b2d8742995
raiponz <spooki.leserpent@orange.fr>
2018-08-11 11:35:08 +0200

    try loading image

--- Assets/Scripts/IslandGenerator.cs
+++ Assets/Scripts/IslandGenerator.cs
@@ -11,16 +11,55 @@ public class IslandGenerator : MonoBehaviour {
 
     [SerializeField]
     private Tile sandTile;
+    [SerializeField]
+    private Texture2D heigtmap;
 	// Use this for initialization
 	void Start () {
+        int[,] map = new int[,] { { 1, 0 ,1,1}, { 1, 0,0,0 }, { 1, 1,0,1 }, { 1, 1,0,1 } }; ;
+        ReadImage();
         Vector3Int vectorPosition = Vector3Int.zero;
-        for (int i = 0; i< 5; i++) {
-            vectorPosition.x += 1; 
-            Debug.Log(vectorPosition.x);
-            groundTileMap.SetTile(vectorPosition, sandTile);
+        for (int i = 0; i< 4; i++) {
+            for(int j = 0; j < 4; j++)
+            {
+                if(map[i, j] == 1)
+                {
+                    vectorPosition.x = i;
+                    vectorPosition.y = j;
+                    Debug.Log(vectorPosition.x);
+                    
+                    groundTileMap.SetTile(vectorPosition, sandTile);
+                }
+               
+            }
+           
         }
 
 	}
+
+    private void ReadImage()
+    {
+        Texture2D image = heigtmap;
+        Debug.Log(image);
+
+        // Iterate through it's pixels
+        for (int i = 0; i < image.width; i++)
+        {
+            for (int j = 0; j < image.height; j++)
+            {
+                Color pixel = image.GetPixel(i, j);
+
+                // if it's a white color then just debug...
+                if (pixel == Color.white)
+                {
+                    Debug.Log("Im white");
+                }
+                else
+                {
+                    Debug.Log("Im black");
+                }
+            }
+        }
+    }
 	
 
 }
