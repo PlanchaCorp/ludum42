72324bcce75134a3f522873ab7524b4df7ba797e
zessirb <zessirb@gmail.com>
2018-08-11 15:36:27 +0200

    Added cursor :chicken:

--- Assets/Scripts/PlayerAction.cs
+++ Assets/Scripts/PlayerAction.cs
@@ -10,14 +10,27 @@ public class PlayerAction : MonoBehaviour {
     public float actionRange = 3.5f;
 
     /// <summary>
-    /// Tilemap of the terrain
+    /// Tilemaps of the terrains
     /// </summary>
-    private Tilemap terrainTilemap;
+    private List<Tilemap> terrainTilemaps = new List<Tilemap>();
+    private Grid grid;
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
-        terrainTilemap = GameObject.FindGameObjectWithTag("TerrainTilemap").GetComponent<Tilemap>();
+        StartCoroutine(LateStart(0.1f));
+    }
+
+    IEnumerator LateStart(float waitTime) {
+        yield return new WaitForSeconds(waitTime);
+        GameObject[] terrainsObjects = GameObject.FindGameObjectsWithTag("TerrainTilemap");
+        int i = 0;
+        foreach (GameObject terrainObject in terrainsObjects)
+        {
+            terrainTilemaps.Add(terrainObject.GetComponent<Tilemap>());
+            i++;
+        }
+        grid = GameObject.FindGameObjectWithTag("Grid").GetComponent<Grid>();
     }
 	
 	// Update is called once per frame
@@ -27,14 +40,32 @@ public class PlayerAction : MonoBehaviour {
     }
 
     /// <summary>
+    /// Place an indicator on the tile hovered by the cursor
+    /// </summary>
+    private void HoverTile(Vector3 mousePosition)
+    {
+        foreach (Tilemap terrainTilemap in terrainTilemaps)
+        {
+            Vector3Int cellPosition = terrainTilemap.WorldToCell(mousePosition);
+            TileBase tile = terrainTilemap.GetTile(cellPosition);
+            if (tile != null)
+            {
+                GameObject tileSelection = GameObject.FindGameObjectWithTag("TileSelection");
+                tileSelection.transform.position = terrainTilemap.CellToLocal(cellPosition);
+            }
+        }
+    }
+
+    /// <summary>
     /// Handle click interaction
     /// </summary>
     private void InteractWithEnvironment()
     {
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouseWorldPosition.z = 0;
+        HoverTile(mouseWorldPosition);
         if (Input.GetMouseButtonDown(0))
         {
-            Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            mouseWorldPosition.z = 0;
             InteractWithTerrain(mouseWorldPosition);
         }
     }
@@ -45,18 +76,22 @@ public class PlayerAction : MonoBehaviour {
     /// <param name="mousePosition">Position where to interact</param>
     private void InteractWithTerrain(Vector3 mousePosition)
     {
-        Vector3Int cellPosition = terrainTilemap.WorldToCell(mousePosition);
-        TileBase tile = terrainTilemap.GetTile(cellPosition);
-        if (tile != null)
+        foreach (Tilemap terrainTilemap in terrainTilemaps)
         {
-            Vector3 tileDistanceVector = cellPosition - transform.position;
-            float tileDistance = Mathf.Sqrt(Mathf.Pow(tileDistanceVector.x, 2) + Mathf.Pow(tileDistanceVector.y, 2));
-            if (tileDistance <= actionRange)
+            Vector3Int cellPosition = terrainTilemap.WorldToCell(mousePosition);
+            TileBase tile = terrainTilemap.GetTile(cellPosition);
+            if (tile != null)
             {
-                Debug.Log("Interaction");
-            } else
-            {
-                gameObject.GetComponent<PlayerMovement>().SetMouseClick(mousePosition);
+                Vector3 tileDistanceVector = cellPosition - transform.position;
+                float tileDistance = Mathf.Sqrt(Mathf.Pow(tileDistanceVector.x, 2) + Mathf.Pow(tileDistanceVector.y, 2));
+                if (tileDistance <= actionRange)
+                {
+                    Debug.Log("Interaction : " + tile.name);
+                }
+                else
+                {
+                    gameObject.GetComponent<PlayerMovement>().SetMouseClick(mousePosition);
+                }
             }
         }
     }
