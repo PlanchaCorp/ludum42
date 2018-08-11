22faff887099bdf8c6011b408969f1f47e73957b
raiponz <spooki.leserpent@orange.fr>
2018-08-11 11:00:01 +0200

    create Island generation script

---
+++ Assets/Scripts/IslandGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class IslandGenerator : MonoBehaviour {
+
+
+    [SerializeField]
+    private Tilemap groundTileMap;
+
+    [SerializeField]
+    private Tile sandTile;
+	// Use this for initialization
+	void Start () {
+        Vector3Int vectorPosition = Vector3Int.zero;
+        for (int i = 0; i< 5; i++) {
+            vectorPosition.x += 1; 
+            Debug.Log(vectorPosition.x);
+            groundTileMap.SetTile(vectorPosition, sandTile);
+        }
+
+	}
+	
+
+}
