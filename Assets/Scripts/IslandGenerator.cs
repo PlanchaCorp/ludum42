5830367e0a08bb7e2d860bba5cc34f8b8377dba2
raiponz <spooki.leserpent@orange.fr>
2018-08-11 15:26:57 +0200

    :v:

--- Assets/Scripts/IslandGenerator.cs
+++ Assets/Scripts/IslandGenerator.cs
@@ -67,11 +67,13 @@ public class IslandGenerator : MonoBehaviour
         for (int l =0; l< scale; l++) {
             GameObject sandLayer = new GameObject();
             sandLayer.AddComponent<Tilemap>();
-            sandLayer.AddComponent<TilemapRenderer>();
+            sandLayer.AddComponent<TilemapRenderer>().sortingLayerName = "Terrain";
             sandLayer.GetComponent<TilemapRenderer>().sortingOrder = l;
             sandLayer.GetComponent<Tilemap>().tileAnchor = Vector3.zero;
+            sandLayer.tag = "TerrainTilemap";
+            
             sandLayer.name = "SandLayer" + l;
-            sandLayer.GetComponent<TilemapRenderer>().sortOrder = TilemapRenderer.SortOrder.TopLeft;
+            sandLayer.GetComponent<TilemapRenderer>().sortOrder = TilemapRenderer.SortOrder.BottomLeft;
             sandLayer.transform.SetParent(groundTileMap.transform);
             
         }
