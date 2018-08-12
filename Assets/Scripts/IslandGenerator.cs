9186a125d672be73e4cc7eac7eef847d506e64a7
raiponz <spooki.leserpent@orange.fr>
2018-08-12 02:26:47 +0200

    update perlin noise

--- Assets/Scripts/IslandGenerator.cs
+++ Assets/Scripts/IslandGenerator.cs
@@ -27,8 +27,8 @@ public class IslandGenerator : MonoBehaviour
     [SerializeField]
     private AnimatedTile waterTile;
 
-    private int perlinSizeX ;// Longueur du tableau
-    private int perlinSizeY ;// Largeur du tableau 
+    private int perlinSizeX;// Longueur du tableau
+    private int perlinSizeY;// Largeur du tableau 
 
     public int mapSize = 40;
 
@@ -52,7 +52,7 @@ public class IslandGenerator : MonoBehaviour
     // Use this for initialization
     void Awake()
     {
-        
+
         perlinSizeX = mapSize;// Longueur du tableau
         perlinSizeY = mapSize;// Largeur du tableau 
         K = 0.01f;
@@ -80,56 +80,60 @@ public class IslandGenerator : MonoBehaviour
             {
                 mask[x, y] = Mathf.RoundToInt(map[x, y] * 7 * Mathf.Exp(-((x - X0) * (x - X0) + (y - Y0) * (y - Y0)) * K));
             }
-
         }
         return mask;
     }
 
     private void GenerateIsland(int[,] map)
     {
-        for (int l =0; l< scale; l++) {
+        for (int l = 0; l < scale; l++)
+        {
             GameObject sandLayer = new GameObject();
             sandLayer.AddComponent<Tilemap>();
             sandLayer.AddComponent<TilemapRenderer>().sortingLayerName = "Terrain";
             sandLayer.GetComponent<TilemapRenderer>().sortingOrder = l;
-            sandLayer.GetComponent<Tilemap>().tileAnchor = new Vector3(0f,0f);
+            sandLayer.GetComponent<Tilemap>().tileAnchor = new Vector3(0f, 0f);
             sandLayer.GetComponent<Tilemap>().orientation = Tilemap.Orientation.Custom;
-            sandLayer.transform.position = new Vector3(0f, l/10f, 0f);
-            
+            sandLayer.transform.position = new Vector3(0f, l / 10f, 0f);
+
             sandLayer.tag = "TerrainTilemap";
-            
+
             sandLayer.name = "SandLayer" + l;
             sandLayer.GetComponent<TilemapRenderer>().sortOrder = TilemapRenderer.SortOrder.TopLeft;
             sandLayer.transform.SetParent(groundTileMap.transform);
-           
+
         }
         Vector3Int vectorPosition = Vector3Int.zero;
-        float y = 0.0F;
+        
         for (int i = 0; i < perlinSizeX; i++)
         {
-            float x = 0.0F;
+           
             for (int j = 0; j < perlinSizeY; j++)
             {
-                vectorPosition.x = mapSize/2-  i;
+                vectorPosition.x = mapSize / 2 - i;
                 vectorPosition.y = mapSize / 2 - j;
 
-                float xCoord = xOrg + x / perlinSizeX * scale;
-                float yCoord = yOrg + y / perlinSizeY * scale;
-               
-                if (Mathf.PerlinNoise(xCoord + seed, yCoord + seed) < 0.25f)
-                {
-                    
-                    Tile tile = sandTiles[map[i, j]];
+                float xCoord = (xOrg + i / (float) perlinSizeX)* scale;
+                float yCoord = (yOrg + j / (float) perlinSizeY) * scale;
                 
-                groundTileMap.transform.Find("SandLayer" + map[i, j]).GetComponent<Tilemap>().SetTile(vectorPosition, tile);
-                 }
-                 else
-                 {
-                     groundTileMap.transform.Find("SandLayer" + map[i, j]).GetComponent<Tilemap>().SetTile(vectorPosition, sandTiles[map[i, j]]);
-                 }
+                Tile tile;
+                Debug.Log(xCoord+" "+ yCoord +" "+Mathf.PerlinNoise((float)xCoord + seed, (float)yCoord + seed));
+                if (Mathf.PerlinNoise(xCoord + seed, yCoord + seed) > 0.25f)
+                {
 
-               
-             
+                    tile = sandTiles[map[i, j]];
+
+
+                }
+                else
+                {
+                    tile = stoneTiles[map[i, j]];
+
+                }
+                groundTileMap.transform.Find("SandLayer" + map[i, j]).GetComponent<Tilemap>().SetTile(vectorPosition, tile);
+
+
+              
             }
         }
     }
